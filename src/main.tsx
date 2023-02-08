6133cafe37b70e4c958b1d5482f2fc9591488582
Gabriel <thegabrielrockson@gmail.com>
2023-02-08 06:51:07 +0000

    :fire: Remove unused css style files and imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import React, { Children } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
 import Home from "./pages/Home";
 
 import theme from "./theme";
